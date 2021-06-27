3908ff094f5dc59339bc78e4188a259211b4ce3b
pilau rice <minxter.yt@gmail.com>
2021-06-27 16:17:30 +0000

    links fixed

--- commands/lookup.js
+++ commands/lookup.js
@@ -4,65 +4,65 @@ const Discord = require('discord.js');
 
 
 function truncateString(str, num) {
-    try {
-        if (str.length <= num) {
-            return str
-        }
-        return str.slice(0, num) + '..'
-    } catch (err) {
-        str = `No Description..`
-        return str
+  try {
+    if (str.length <= num) {
+      return str
     }
+    return str.slice(0, num) + '..'
+  } catch (err) {
+    str = `No Description..`
+    return str
+  }
 }
 
 module.exports = {
-    name: "search",
-    description: "Search for a package",
-    run: async({ client, message }, args) => {
-        let btn1 = new MessageButton().setEmoji('<:prev:855836521110044684>').setStyle('grey').setID('prev');
-        let btn2 = new MessageButton().setEmoji('<:next:855836521402990592>').setStyle('grey').setID('next');
-        let row = new MessageActionRow().addComponent(btn1).addComponent(btn2);
-        packageName = args.join(" ").replace(new RegExp("@", "g"), '')
-        if (packageName.length == 0) { message.channel.send("Specify a package name"); return }
+  name: "search",
+  description: "Search for a package",
+  run: async ({ client, message }, args) => {
+    let btn1 = new MessageButton().setEmoji('<:prev:855836521110044684>').setStyle('grey').setID('prev');
+    let btn2 = new MessageButton().setEmoji('<:next:855836521402990592>').setStyle('grey').setID('next');
+    let row = new MessageActionRow().addComponent(btn1).addComponent(btn2);
+    packageName = args.join(" ").replace(new RegExp("@", "g"), '')
+    if (packageName.length == 0) { message.channel.send("Specify a package name"); return }
 
 
-        body = await fetch(`https://api.parcility.co/db/search?q=${packageName}`).then(res => res.json())
+    body = await fetch(`https://api.parcility.co/db/search?q=${packageName}`).then(res => res.json())
 
-        if (!body.status) {
-            message.reply("Nothing found with given params or parcility couldn't be reached.");
-            return
-        } else {
+    if (!body.status) {
+      message.reply("Nothing found with given params or parcility couldn't be reached.");
+      return
+    } else {
 
-            package = body.data[0];
+      package = body.data[0];
 
-            result = {
-                title: package.Name,
-                description: truncateString(package.Description, 100),
-                url: `https://parcility.co/package/${package.Package}`,
-                fields: [{
-                        name: `Add Repository`,
-                        value: `[Open In Safari](https://package-manager-links.minxteryt.repl.co/${package.repo.url})`,
-                        inline: true,
-                    },
-                    {
-                        name: `Section`,
-                        value: package.Section,
-                        inline: true,
-                    }
-                ],
-                thumbnail: {
-                    url: `https://api.parcility.co/db/package/${package.Package}/Icon`
-                },
-                footer: {
-                    text: "Showing " + 1 + "/" + body.data.length + " results for " + packageName,
-                    icon_url: package.repo.icon
-                }
-            };
-
-            message.channel.send({
-                components: row,
-                embeds: result
-            })
+      result = {
+        title: package.Name,
+        description: truncateString(package.Description, 100),
+        url: `https://parcility.co/package/${package.Package}`,
+        fields: [{
+          name: `Add Repository`,
+          value: `[Open In Safari](https://package-manager-links.minxteryt.repl.co/?repo=${package.repo.url})`,
+          inline: true,
+        },
+        {
+          name: `Section`,
+          value: package.Section,
+          inline: true,
         }
+        ],
+        thumbnail: {
+          url: `https://api.parcility.co/db/package/${package.Package}/Icon`
+        },
+        footer: {
+          text: "Showing " + 1 + "/" + body.data.length + " results for " + packageName,
+          icon_url: package.repo.icon
+        }
+      };
+
+      message.channel.send({
+        components: row,
+        embeds: result
+      })
     }
+  }
 };
--- events/clickButton.js
+++ events/clickButton.js
@@ -4,79 +4,79 @@ const Discord = require('discord.js');
 
 
 function truncateString(str, num) {
-    try {
-        if (str.length <= num) {
-            return str
-        }
-        return str.slice(0, num) + '..'
-    } catch (err) {
-        str = `No Description..`
-        return str
+  try {
+    if (str.length <= num) {
+      return str
     }
+    return str.slice(0, num) + '..'
+  } catch (err) {
+    str = `No Description..`
+    return str
+  }
 }
 
 module.exports = {
-    name: "clickButton",
-    once: false,
-    run: async(client, button) => {
-        let btn1 = new MessageButton().setEmoji('<:prev:855836521110044684>').setStyle('grey').setID('prev');
-        let btn2 = new MessageButton().setEmoji('<:next:855836521402990592>').setStyle('grey').setID('next');
-        let row = new MessageActionRow().addComponent(btn1).addComponent(btn2);
+  name: "clickButton",
+  once: false,
+  run: async (client, button) => {
+    let btn1 = new MessageButton().setEmoji('<:prev:855836521110044684>').setStyle('grey').setID('prev');
+    let btn2 = new MessageButton().setEmoji('<:next:855836521402990592>').setStyle('grey').setID('next');
+    let row = new MessageActionRow().addComponent(btn1).addComponent(btn2);
 
-        packageName = button.message.embeds[0].footer.text.split(`for `)[1]
-        index = button.message.embeds[0].footer.text.split("Showing ")[1].split("/")[0] - 1
+    packageName = button.message.embeds[0].footer.text.split(`for `)[1]
+    index = button.message.embeds[0].footer.text.split("Showing ")[1].split("/")[0] - 1
 
-        body = await fetch(`https://api.parcility.co/db/search?q=${packageName}`).then(res => res.json())
-        if (!body.status) {
-            message.reply("Nothing found with given params or parcility couldn't be reached.");
-            return
-        } else {
+    body = await fetch(`https://api.parcility.co/db/search?q=${packageName}`).then(res => res.json())
+    if (!body.status) {
+      message.reply("Nothing found with given params or parcility couldn't be reached.");
+      return
+    } else {
 
 
-            if (button.id == "prev" && (index - 1) >= 0) {
-                newindex = parseInt(index) - 1
-            } else if (button.id == "next" && (index + 1) <= (body.data.length - 1)) {
-                newindex = parseInt(index) + 1
-            } else {
-                newindex = index
-            }
+      if (button.id == "prev" && (index - 1) >= 0) {
+        newindex = parseInt(index) - 1
+      } else if (button.id == "next" && (index + 1) <= (body.data.length - 1)) {
+        newindex = parseInt(index) + 1
+      } else {
+        newindex = index
+      }
 
-            package = body.data[newindex];
+      package = body.data[newindex];
 
-            smallResult = {
-                title: package.Name,
-                description: truncateString(package.Description, 100),
-                url: `https://parcility.co/package/${package.Package}`,
-                fields: [{
-                        name: `Add Repository`,
-                        value: `[Open In Safari](https://package-manager-links.minxteryt.repl.co/${package.repo.url})`,
-                        inline: true,
-                    },
-                    {
-                        name: `Section`,
-                        value: package.Section,
-                        inline: true,
-                    }
-                ],
-                thumbnail: {
-                    url: `https://api.parcility.co/db/package/${package.Package}/Icon`
-                },
-                footer: {
-                    text: "Showing " + parseInt(newindex + 1) + "/" + body.data.length + " results for " + packageName,
-                    icon_url: package.repo.icon
-                }
-            };
-
-            try {
-                button.edit({
-                    content: smallResult,
-                    components: row
-                })
-            } catch (err) {
-                console.log(err)
-                button.defer()
-            }
-
+      smallResult = {
+        title: package.Name,
+        description: truncateString(package.Description, 100),
+        url: `https://parcility.co/package/${package.Package}`,
+        fields: [{
+          name: `Add Repository`,
+          value: `[Open In Safari](https://package-manager-links.minxteryt.repl.co/?repo=${package.repo.url})`,
+          inline: true,
+        },
+        {
+          name: `Section`,
+          value: package.Section,
+          inline: true,
         }
+        ],
+        thumbnail: {
+          url: `https://api.parcility.co/db/package/${package.Package}/Icon`
+        },
+        footer: {
+          text: "Showing " + parseInt(newindex + 1) + "/" + body.data.length + " results for " + packageName,
+          icon_url: package.repo.icon
+        }
+      };
+
+      try {
+        button.edit({
+          content: smallResult,
+          components: row
+        })
+      } catch (err) {
+        console.log(err)
+        button.defer()
+      }
+
     }
+  }
 }
